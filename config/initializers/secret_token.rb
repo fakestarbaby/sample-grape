27d8834b9ef866a219db0808187887420c444768
fakestarbaby <fakestarbaby@gmail.com>
2012-08-01 10:39:22 +0900

    Added rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleGrape::Application.config.secret_token = 'bfc9eceafb30c54ba24016ae4853407d890a2db6212d6433edef003b66e0020f81cceb9df007437f80653d53b8997a8d607b678a0d1a9340bd52829556d0ce12'
